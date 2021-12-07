6f6368f20757e56818abc0620118d1548a365e48
theBlue0x <3836882+theBlue0x@users.noreply.github.com>
2021-12-07 17:15:46 -0500

    new currencies

--- trade/js/constants.js
+++ trade/js/constants.js
@@ -1,5 +1,13 @@
 var Constants = (function(Constants, $, undefined) {
+  
   Constants.USDXId = '9654905020611430837';
+
+  Constants.AUDXId = '5228498090188867983';
+
+  Constants.CADXId = '4167983987313144762';
+
   Constants.apiUrl = 'https://wallet.blue0x.com/nxt';
+
   return Constants;
+
 } (Constants || {}, jQuery));
